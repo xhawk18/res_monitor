baa647d8fe142d77dd2e0d6b3172e6a8317f1cd3
X-Hawk <xhawk18@gmail.com>
2025-04-13 12:48:26 +0800

    加入rotate日志

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,50 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 #include <chrono>
 #include <thread>
+#include <filesystem>
+#include <csignal>
+#include <atomic>
+
+namespace fs = std::filesystem;
+
+std::atomic<bool> g_stopping{false};
+std::mutex g_mutex;
+std::condition_variable g_cv;
+
+void signalHandler(int signum) {
+    g_stopping = true;
+    g_cv.notify_all();  // 唤醒所有等待的线程
+}
 
 int main() {
-    // 创建控制台和文件日志器
+    // 注册信号处理
+    std::signal(SIGINT, signalHandler);
+    std::signal(SIGTERM, signalHandler);
+
+    // 创建控制台和轮转文件日志器
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("/home/user/Work/res_monitor/logs/monitor.log");
+
+    // 获取可执行文件当前目录
+    fs::path currentPath = std::filesystem::current_path();
+    fs::path logPath = currentPath / "logs";
+    // 创建轮转文件日志器
+    std::filesystem::create_directories(logPath);
+    fs::path logFilePath = logPath / "monitor.log";
+
+    auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        logFilePath, 
+        50 * 1024 * 1024, // 每个文件最大50MB
+        10  // 保留10个文件
+    );
     
-    spdlog::logger logger("multi_sink", {console_sink, file_sink});
+    spdlog::logger logger("multi_sink", {console_sink, rotating_sink});
     logger.set_level(spdlog::level::info);
 
     ResourceMonitor monitor;
-    while(true) {
+    while(!g_stopping) {
         auto cpuUsage = monitor.getCpuUsage();
         auto memUsage = monitor.getMemoryUsage();
         auto diskIo = monitor.getDiskIo();
@@ -36,8 +67,15 @@ int main() {
         for(const auto& process : topDiskIos) {
             logger.info("Top Disk: {}", process);
         }
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        // 可中断的睡眠
+        std::unique_lock<std::mutex> lock(g_mutex);
+        g_cv.wait_for(lock, std::chrono::seconds(10), []{ return g_stopping.load(); });
+        //std::this_thread::sleep_for(std::chrono::seconds(10));
     }
 
+    logger.info("Stopping...");
+    logger.flush();
+    spdlog::shutdown();
     return 0;
 }
